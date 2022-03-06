72975c2aade11a92508227ffd86a1d7c072265dc
Laurence Davies <ljd.davies@gmail.com>
2022-03-06 14:50:49 +0200

    fix npm imports

--- src/investec-api.ts
+++ src/investec-api.ts
@@ -2,4 +2,5 @@ import {InvestecClient} from './investec/InvestecClient'
 import {getTransactionHash} from './investec/utils'
 
 export * from './investec/apiTypes'
+export * from './investec/types'
 export {InvestecClient, getTransactionHash}
